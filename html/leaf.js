9c2cb933ccda5982e1643daaca6b58d1faa1374e
leongt1 <32676845+leongt1@users.noreply.github.com>
2017-11-11 16:45:53 -0500

    registry

--- html/leaf.js
+++ html/leaf.js
@@ -5,9 +5,22 @@ $(document).ready(function() {
 });
 
 function validate(formObj) {
-  if(!formObj.email.value.includes("@") || !formObj.email.value.includes(".")) {
-     alert("You must enter a valid email.")
-     }
+  if(formObj.firstName.value == "") {
+    alert("You must enter a first name.");
+    return false;
+  }
+  if(formObj.lastName.value == "") {
+    alert("You must enter a last name.");
+    return false;
+  }
+  if(formObj.email.value == "" || !formObj.email.value.includes("@") || !formObj.email.value.includes(".")) {
+    alert("You must enter a valid email.");
+    return false;
+  }
+  if(formObj.username.value == "") {
+    alert("You must enter a username.");
+    return false;
+  }
   if(formObj.password.value.length < 6) {
     alert("Password must be greater then 6 characters.");
     return false;
