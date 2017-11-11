fbe75d586e99e20bca91288472e6bfce1dd9a0c6
leongt1 <32676845+leongt1@users.noreply.github.com>
2017-11-11 15:41:19 -0500

    login updates

--- html/leaf.js
+++ html/leaf.js
@@ -3,3 +3,12 @@ $(document).ready(function() {
     $("#output").html($("#message").val());
   });
 });
+
+function validate(formObj) {
+  if(formObj.password.length < 6) {
+    alert("Password must be greater then 6 characters");
+    return false;
+  }
+  alert("You have submitted a valid form.");  
+  return false;
+}
