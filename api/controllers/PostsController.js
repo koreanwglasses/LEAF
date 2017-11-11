8d1080c14a21ad56f314ae95af64f2a6c69d236d
eagraf <graf.ethan@gmail.com>
2017-11-11 13:49:36 -0500

    Added some basic routing.

--- api/controllers/PostsController.js
+++ api/controllers/PostsController.js
@@ -6,6 +6,18 @@
  */
 
 module.exports = {
+    getPost: function (req, res) {
+        return res.send('Get Post');
+    },
+    getThread: function (req, res) {
+        return res.send('Get Thread');
+    },
+    push: function (req, res) {
+        return res.send('Push');
+    },
+    branch: function (req, res) {
+        return res.send('Branch');
+    }
 	
 };
 
