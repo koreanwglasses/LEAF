4f73d21048109f628a723835fa2585a3899a523c
Fred Choi <koreanwglasses@gmail.com>
2017-11-11 13:38:09 -0500

    Added interface prototypes

--- api/controllers/PostsController.js
+++ api/controllers/PostsController.js
@@ -6,6 +6,15 @@
  */
 
 module.exports = {
-	
+    //  no need for GET, since everything is dynamically updated with AJAX 
+    //  anyway, and CRUD is implemented by default.
+
+    branch: function(req, res) {
+
+    },
+
+    push: function(req, res) {
+
+    }
 };
 
