ded7b2409bba165e5f34eb2a860e312110791662
Fred Choi <koreanwglasses@gmail.com>
2017-11-11 12:27:51 -0500

    Initialized Posts and Users apis

---
+++ api/models/Posts.js
@@ -0,0 +1,22 @@
+/**
+ * Posts.js
+ *
+ * @description :: TODO: You might write a short summary of how this model works and what it represents here.
+ * @docs        :: http://sailsjs.org/documentation/concepts/models-and-orm/models
+ */
+
+module.exports = {
+
+    attributes: {
+        content: { type: 'text' },
+    },
+
+    getParent: function(options, cb) {
+
+    },
+
+    getChildren: function(options, cb) {
+
+    }
+};
+
