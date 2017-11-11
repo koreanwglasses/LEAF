bf63a40016f15c14fce6148b5d81c5cf40cbae37
leongt1 <32676845+leongt1@users.noreply.github.com>
2017-11-11 15:57:51 -0500

    email check validator

--- html/leaf.js
+++ html/leaf.js
@@ -5,8 +5,11 @@ $(document).ready(function() {
 });
 
 function validate(formObj) {
+  if(!formObj.email.value.includes("@") || !formObj.email.value.includes(".")) {
+     alert("You must enter a valid email.")
+     }
   if(formObj.password.value.length < 6) {
-    alert("Password must be greater then 6 characters");
+    alert("Password must be greater then 6 characters.");
     return false;
   }
   alert("You have submitted a valid form.");  
