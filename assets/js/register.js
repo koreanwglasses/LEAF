d48a3ebd61715949a2a60736f208fc9cd89408e5
Andrew Qu <32676955+qua5@users.noreply.github.com>
2017-11-11 19:31:37 -0500

    added register.js

---
+++ assets/js/register.js
@@ -0,0 +1,15 @@
+function register(){
+  $.ajax({
+    url: 'http://localhost:1337/register/user',
+    data:{
+      firstname: $("#firstName").val(),
+      lastname: $("#lastName").val(),
+      email: $("#email").val(),
+      username: $("#username").val(),
+      password: $("#password").val()
+      },
+    dataType: 'applications/json'
+    }
+  });
+
+}
